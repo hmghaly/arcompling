9f55e24cde8f4038d3266ff8ea50a092e42b72f5
hmghaly <hmghaly@users.noreply.github.com>
2017-02-10 21:40:30 -0500

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,23 @@
+year_born=2005
+year_current=2017
+
+age=year_current-year_born
+
+print "person's age is", age
+
+first_name="Omar"
+last_name="Adam"
+
+full_name=first_name+" "+last_name
+
+print full_name
+
+people1=["Ali","Adam","Omar","Samir"]
+people2=["Karim","Amr","Hassan"]
+
+all_people=people1+people2
+
+print all_people
+
+people_age_dictionary={"Adam":18,"Ali":25,"Samir":32}
+print "Ali's age is:", people_age_dictionary["Ali"]
